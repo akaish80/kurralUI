ad1d45b3381ff4ff089e2af800427d71039961c8
ArunKumar <ArunKumar.D@cognizant.com>
2021-02-02 17:39:13 -0500

    Initial commit

---
+++ KurralUI/Controller/KurralListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  KurralListViewController.swift
+//  KurralUI
+//
+//  Created by M_243704 on 1/26/21.
+//
+
+import UIKit
+
+class KurralListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
